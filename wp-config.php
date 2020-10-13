7761fc56faebd271960ad169e094614b8798855d
drosar1o <58198299+drosar1o@users.noreply.github.com>
2020-10-13 00:49:39 -0400

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -23,7 +23,7 @@
 define( 'DB_NAME', 'wordpress' );
 
 /** MySQL database username */
-define( 'DB_USER', 'drosario' );
+define( 'DB_USER', 'wordpress' );
 
 /** MySQL database password */
 define( 'DB_PASSWORD', 'Fullsail1!' );
